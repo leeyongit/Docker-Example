4094e9cfd0dfd4254a8ba7762f238ba6b0d0eb48
李永 <leeyongit@163.com>
2014-07-03 09:55:24 +0800

    Create tree.php
    分类树

---
+++ tree.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+  对象版
+  $items = array(
+          (object) array('id' => 42, 'parent_id' => 1),
+          (object) array('id' => 43, 'parent_id' => 42),
+          (object) array('id' => 1,  'parent_id' => 0),
+  );
+*/
+function buildTree($items) {
+
+    $childs = array();
+
+    foreach($items as $item)
+        $childs[$item->parent_id][] = $item;
+
+    foreach($items as $item) if (isset($childs[$item->id]))
+        $item->childs = $childs[$item->id];
+
+    return $childs[0];
+}
+
+$tree = buildTree($items);
+/*
+Output of print_r($tree):
+
+stdClass Object
+(
+    [id] => 1
+    [parent_id] => 0
+    [childs] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [id] => 42
+                    [parent_id] => 1
+                    [childs] => Array
+                        (
+                            [0] => stdClass Object
+                                (
+                                    [id] => 43
+                                    [parent_id] => 42
+                                )
+
+                        )
+
+                )
+
+        )
+
+)
+*/
+
+// 数组版
+function buildTree($items) {
+
+    $childs = array();
+
+    foreach($items as &$item) $childs[$item['parent_id']][] = &$item;
+    unset($item);
+
+    foreach($items as &$item) if (isset($childs[$item['id']]))
+            $item['childs'] = $childs[$item['id']];
+
+    return $childs[0];
+}
+
+$tree = buildTree($items);
