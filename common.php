d85e443b5c989c02e9ffc621eec42965d4ea9f4c
leeyongit <leeyongit@163.com>
2015-09-16 00:59:34 +0800

    公共函数

--- common.php
+++ common.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * 常用php公共函数
+ * 常用php公共函数 验证与安全
  */
+
+
+
+/**
+ * 检查用户名是否符合规定
+ *
+ * @param STRING $username 要检查的用户名
+ * @return 	TRUE or FALSE
+ */
+function is_username($username) {
+	$strlen = strlen($username);
+	if(is_badword($username) || !preg_match("/^[a-zA-Z0-9_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]+$/", $username)){
+		return false;
+	} elseif ( 20 < $strlen || $strlen < 2 ) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * 判断email格式是否正确
+ * @param $email
+ */
+function is_email($email) {
+	return strlen($email) > 6 && preg_match("/^[\w\-\.]+@[\w\-\.]+(\.\w+)+$/", $email);
+}
 
 /**
  * 验证输入的邮件地址是否合法
@@ -11,12 +37,12 @@
  *
  * @return bool
  */
-function is_email($user_email)
+function is_email($email)
 {
     $chars = "/^([a-z0-9+_]|\\-|\\.)+@(([a-z0-9_]|\\-)+\\.)+[a-z]{2,6}\$/i";
-    if (strpos($user_email, '@') !== false && strpos($user_email, '.') !== false)
+    if (strpos($email, '@') !== false && strpos($email, '.') !== false)
     {
-        if (preg_match($chars, $user_email))
+        if (preg_match($chars, $email))
         {
             return true;
         }
@@ -46,5 +72,254 @@ function is_time($time)
 }
 
 
+/**
+ * 返回经addslashes处理过的字符串或数组
+ * @param $string 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_addslashes($string){
+	if(!is_array($string)) return addslashes($string);
+	foreach($string as $key => $val) $string[$key] = new_addslashes($val);
+	return $string;
+}
 
+/**
+ * 返回经stripslashes处理过的字符串或数组
+ * @param $string 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_stripslashes($string) {
+	if(!is_array($string)) return stripslashes($string);
+	foreach($string as $key => $val) $string[$key] = new_stripslashes($val);
+	return $string;
+}
+
+/**
+ * 返回经htmlspecialchars处理过的字符串或数组
+ * @param $obj 需要处理的字符串或数组
+ * @return mixed
+ */
+function new_html_special_chars($string) {
+	if(!is_array($string)) return htmlspecialchars($string);
+	foreach($string as $key => $val) $string[$key] = new_html_special_chars($val);
+	return $string;
+}
+/**
+ * 安全过滤函数
+ *
+ * @param $string
+ * @return string
+ */
+function safe_replace($string) {
+	$string = str_replace('%20','',$string);
+	$string = str_replace('%27','',$string);
+	$string = str_replace('%2527','',$string);
+	$string = str_replace('*','',$string);
+	$string = str_replace('"','&quot;',$string);
+	$string = str_replace("'",'',$string);
+	$string = str_replace('"','',$string);
+	$string = str_replace(';','',$string);
+	$string = str_replace('<','&lt;',$string);
+	$string = str_replace('>','&gt;',$string);
+	$string = str_replace("{",'',$string);
+	$string = str_replace('}','',$string);
+	$string = str_replace('\\','',$string);
+	return $string;
+}
+
+/**
+ * xss过滤函数
+ *
+ * @param $string
+ * @return string
+ */
+function remove_xss($string) { 
+    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string);
+
+    $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
+
+    $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
+
+    $parm = array_merge($parm1, $parm2); 
+
+	for ($i = 0; $i < sizeof($parm); $i++) { 
+		$pattern = '/'; 
+		for ($j = 0; $j < strlen($parm[$i]); $j++) { 
+			if ($j > 0) { 
+				$pattern .= '('; 
+				$pattern .= '(&#[x|X]0([9][a][b]);?)?'; 
+				$pattern .= '|(&#0([9][10][13]);?)?'; 
+				$pattern .= ')?'; 
+			}
+			$pattern .= $parm[$i][$j]; 
+		}
+		$pattern .= '/i';
+		$string = preg_replace($pattern, '', $string); 
+	}
+	return $string;
+}
+
+
+/**
+ * 转义 javascript 代码标记
+ *
+ * @param $str
+ * @return mixed
+ */
+ function trim_script($str) {
+	if(is_array($str)){
+		foreach ($str as $key => $val){
+			$str[$key] = trim_script($val);
+		}
+ 	}else{
+ 		$str = preg_replace ( '/\<([\/]?)script([^\>]*?)\>/si', '&lt;\\1script\\2&gt;', $str );
+		$str = preg_replace ( '/\<([\/]?)iframe([^\>]*?)\>/si', '&lt;\\1iframe\\2&gt;', $str );
+		$str = preg_replace ( '/\<([\/]?)frame([^\>]*?)\>/si', '&lt;\\1frame\\2&gt;', $str );
+		$str = str_replace ( 'javascript:', 'javascript：', $str );
+ 	}
+	return $str;
+}
+
+
+/**
+ * 获取当前页面完整URL地址
+ */
+function get_url() {
+	$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+	$php_self = $_SERVER['PHP_SELF'] ? safe_replace($_SERVER['PHP_SELF']) : safe_replace($_SERVER['SCRIPT_NAME']);
+	$path_info = isset($_SERVER['PATH_INFO']) ? safe_replace($_SERVER['PATH_INFO']) : '';
+	$relate_url = isset($_SERVER['REQUEST_URI']) ? safe_replace($_SERVER['REQUEST_URI']) : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.safe_replace($_SERVER['QUERY_STRING']) : $path_info);
+	return $sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url;
+}
+
+/**
+ * 字符截取 支持UTF8/GBK
+ * @param $string
+ * @param $length
+ * @param $dot
+ */
+function str_cut($string, $length, $dot = '...') {
+	$strlen = strlen($string);
+	if($strlen <= $length) return $string;
+	$string = str_replace(array(' ','&nbsp;', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), array('∵',' ', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), $string);
+	$strcut = '';
+	if(strtolower(CHARSET) == 'utf-8') {
+		$length = intval($length-strlen($dot)-$length/3);
+		$n = $tn = $noc = 0;
+		while($n < strlen($string)) {
+			$t = ord($string[$n]);
+			if($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
+				$tn = 1; $n++; $noc++;
+			} elseif(194 <= $t && $t <= 223) {
+				$tn = 2; $n += 2; $noc += 2;
+			} elseif(224 <= $t && $t <= 239) {
+				$tn = 3; $n += 3; $noc += 2;
+			} elseif(240 <= $t && $t <= 247) {
+				$tn = 4; $n += 4; $noc += 2;
+			} elseif(248 <= $t && $t <= 251) {
+				$tn = 5; $n += 5; $noc += 2;
+			} elseif($t == 252 || $t == 253) {
+				$tn = 6; $n += 6; $noc += 2;
+			} else {
+				$n++;
+			}
+			if($noc >= $length) {
+				break;
+			}
+		}
+		if($noc > $length) {
+			$n -= $tn;
+		}
+		$strcut = substr($string, 0, $n);
+		$strcut = str_replace(array('∵', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), array(' ', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), $strcut);
+	} else {
+		$dotlen = strlen($dot);
+		$maxi = $length - $dotlen - 1;
+		$current_str = '';
+		$search_arr = array('&',' ', '"', "'", '“', '”', '—', '<', '>', '·', '…','∵');
+		$replace_arr = array('&amp;','&nbsp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;',' ');
+		$search_flip = array_flip($search_arr);
+		for ($i = 0; $i < $maxi; $i++) {
+			$current_str = ord($string[$i]) > 127 ? $string[$i].$string[++$i] : $string[$i];
+			if (in_array($current_str, $search_arr)) {
+				$key = $search_flip[$current_str];
+				$current_str = str_replace($search_arr[$key], $replace_arr[$key], $current_str);
+			}
+			$strcut .= $current_str;
+		}
+	}
+	return $strcut.$dot;
+}
+
+/**
+ * 程序执行时间
+ *
+ * @return	int	单位ms
+ */
+function execute_time() {
+	$stime = explode ( ' ', SYS_START_TIME );
+	$etime = explode ( ' ', microtime () );
+	return number_format ( ($etime [1] + $etime [0] - $stime [1] - $stime [0]), 6 );
+}
+
+/**
+* 产生随机字符串
+*
+* @param    int        $length  输出长度
+* @param    string     $chars   可选的 ，默认为 0123456789
+* @return   string     字符串
+*/
+function random($length, $chars = '123456789abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ') {
+	$hash = '';
+	$max = strlen($chars) - 1;
+	for($i = 0; $i < $length; $i++) {
+		$hash .= $chars[mt_rand(0, $max)];
+	}
+	return $hash;
+}
+
+/**
+* 转换字节数为其他单位
+*
+*
+* @param	string	$filesize	字节大小
+* @return	string	返回大小
+*/
+function sizecount($filesize) {
+	if ($filesize >= 1073741824) {
+		$filesize = round($filesize / 1073741824 * 100) / 100 .' GB';
+	} elseif ($filesize >= 1048576) {
+		$filesize = round($filesize / 1048576 * 100) / 100 .' MB';
+	} elseif($filesize >= 1024) {
+		$filesize = round($filesize / 1024 * 100) / 100 . ' KB';
+	} else {
+		$filesize = $filesize.' Bytes';
+	}
+	return $filesize;
+}
+
+/**
+ * 文件下载
+ * @param $filepath 文件路径
+ * @param $filename 文件名称
+ */
+
+function file_down($filepath, $filename = '') {
+	if(!$filename) $filename = basename($filepath);
+	if(is_ie()) $filename = rawurlencode($filename);
+	$filetype = fileext($filename);
+	$filesize = sprintf("%u", filesize($filepath));
+	if(ob_get_length() !== false) @ob_end_clean();
+	header('Pragma: public');
+	header('Last-Modified: '.gmdate('D, d M Y H:i:s') . ' GMT');
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Cache-Control: pre-check=0, post-check=0, max-age=0');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Encoding: none');
+	header('Content-type: '.$filetype);
+	header('Content-Disposition: attachment; filename="'.$filename.'"');
+	header('Content-length: '.$filesize);
+	readfile($filepath);
+	exit;
+}
 ?>
