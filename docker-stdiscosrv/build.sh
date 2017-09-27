44054e8d0f7d2c13d1fae96d74f5197907cd20ce
李永 <leeyong@liyongdeMacBook-Pro.local>
2017-09-27 23:39:41 +0800

    My Dockerfile

--- docker-stdiscosrv/build.sh
+++
@@ -1,6 +0,0 @@
-chomd -R 777 src
-docker build -t syncthing-discovery-server:0.1 .
-syncthing -generate="~/disco_cert/"
-chmod -R 777 ~/disco_cert/
-docker run -p 8443:8443 -v ~/disco_cert:/cert --restart=always syncthing-discovery-server:0.1
-# https://(ServerAddress):8443/v2/?id=(Server device ID above)
