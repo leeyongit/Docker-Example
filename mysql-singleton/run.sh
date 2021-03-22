772341b915e3bcc96fccfe8711d46e1422ec61dc
李永 <leeyongit@163.com>
2021-03-22 19:21:53 +0800

    Update

--- mysql-singleton/run.sh
+++ mysql-singleton/run.sh
@@ -1,3 +1,6 @@
 docker pull mysql:5.7
-docker run --name ibus-mysql -v /data/docker/docker-data/mysql/mysql_data:/var/lib/mysql \
+docker run -p 3306:3306 \
+    -v /data/docker/docker-data/mysql/mysql_data:/var/lib/mysql \
+    -v /data/my.cnf:/etc/mysql/my.cnf \
+    --name ibus-mysql \
     -e MYSQL_ROOT_PASSWORD=ibus@2021#! -d mysql:5.7
